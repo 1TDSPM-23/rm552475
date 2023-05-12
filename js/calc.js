bc4b8f368a6d60f8a925f461bc131870fe6e6084
luizPrendin <luizprendinfelipe@gmail.com>
2023-05-12 09:58:47 -0300

    add estilizacao e try catch

--- js/calc.js
+++ js/calc.js
@@ -1,1 +1,45 @@
+function insereNumeroNoVisor(botao,visor){
 
+    if(botao.value == "+" || botao.value =="-" || botao.value == "*" || botao.value =="/"){
+        
+        //Verificando o penultimo caractere do visor utlizando
+        if(visor.value[visor.value.length-1] == "+"|| visor.value[visor.value.length-1] == "-"
+        || visor.value[visor.value.length-1] == "*" || visor.value[visor.value.length-1] == "/"){
+
+                visor.value.slice(1);
+                
+        }
+        else{
+            visor.value += botao.value
+        }
+    }else{
+        visor.value += botao.value
+    }       
+        
+
+}
+const resultadoDasOperacoes =(visor) => {
+    
+    try{
+        visor.value = eval(visor.value);
+    }
+    catch(error){
+        
+        //Verificando o penultimo caractere do visor utlizando
+        if(visor.value[visor.value.length] == "+"|| visor.value[visor.value.length] == "-"
+        || visor.value[visor.value.length] == "*" || visor.value[visor.value.length] == "/"){
+
+            visor.value = visor.value.slice(visor.value.length);
+                
+        }
+        else{
+            visor.value.shift();
+        }
+    }
+    
+    //Utilizando a funcao eval(expressao) obs: transforma as Strings em op math
+
+    //eval(visor).value;
+    
+    
+}
